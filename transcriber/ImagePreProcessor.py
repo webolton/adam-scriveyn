ba12029880a7c20c629981bbe284ffb26d757268
William Bolton <william.ellet@gmail.com>
2018-05-20 19:08:48 -0400

    Add main function to ImagePreProcessor module

--- transcriber/ImagePreProcessor.py
+++ transcriber/ImagePreProcessor.py
@@ -3,5 +3,16 @@ import os, sys
 from PIL import Image
 
 def load_image():
+    """
+    First step in making this program
+    """
     img = Image.open('images/N_73v_74r.JPG')
-    print(img.format, img.size, img.mode)
+    image_info = (img.format, img.size, img.mode)
+    print(image_info)
+    return image_info
+
+def main():
+    load_image()
+
+if __name__ == '__main__':
+    main()
