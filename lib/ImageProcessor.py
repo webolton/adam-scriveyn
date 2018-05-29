042b6235d117d09f2eedf9a2b51309f5ba50f613
William Bolton <william.ellet@gmail.com>
2018-05-29 14:16:50 -0400

    First pass at method for dividing images and test

--- lib/ImageProcessor.py
+++ lib/ImageProcessor.py
@@ -1,16 +1,28 @@
 # Divide MS images into single pages; name images
-import os, sys
-from PIL import Image
+import imageio
 
-def split_and_rename_images(img_path):
+def split_and_rename_image(img_path, output_path, ms_siglum):
     """
     Split and rename images
     """
 
-    img = Image.open(img_path)
-    image_info = (img.format, img.size, img.mode)
-    print(image_info)
-    return image_info
+    verso_no = 2
+    recto_no = 3
+
+    # Load the image
+    img = imageio.imread(img_path)
+    height, width, _ = img.shape
+
+    # Cut the image in half
+    width_cutoff = width // 2
+    side1 = img[:, :width_cutoff, :]
+    side2 = img[:, width_cutoff:, :]
+
+    # Save the two halves
+    imageio.imwrite(f"{output_path}/{ms_siglum}_{verso_no}v.jpg", side1)
+    imageio.imwrite(f"{output_path}/{ms_siglum}_{recto_no}r.jpg", side2)
+
+    return(height, width)
 
 if __name__ == '__main__':
     main()
--- tests/lib/TestImageProcessor.py
+++ tests/lib/TestImageProcessor.py
@@ -1,14 +1,37 @@
 import unittest
+import os
+import glob
+import imageio
 from lib import ImageProcessor
-from lib.ImageProcessor import split_and_rename_images
+from lib.ImageProcessor import split_and_rename_image
 
 class TestImageProcessor(unittest.TestCase):
 
-    def test_split_and_rename_images(self):
+    def test_split_and_rename_image(self):
         img_path = 'tests/mock_data/Matz.jpg'
-        data_list = ('JPEG', (320, 410), 'RGB')
-        res = split_and_rename_images(img_path)
-        self.assertEqual(res, data_list)
+        output_path = 'tests/mock_data/mock_output_data'
+        ms_siglum = 'N'
+
+        split_and_rename_image(img_path, output_path, ms_siglum)
+
+        side1 = imageio.imread(output_path + f"/{ms_siglum}_2v.jpg")
+        side2 = imageio.imread(output_path + f"/{ms_siglum}_3r.jpg")
+
+        original_image = imageio.imread(img_path)
+
+        for side in (side1, side2):
+            test_height = side.shape[0]
+            test_width = side.shape[1]
+
+            self.assertEqual(test_height, original_image.shape[0])
+            self.assertEqual(test_width, original_image.shape[1] // 2)
+
+    def tearDown(self):
+        if glob.glob('tests/mock_data/mock_output_data/*.jpg'):
+            files = glob.glob('tests/mock_data/mock_output_data/*.jpg')
+            for file in files:
+                os.remove(file)
+
 
 if __name__ == '__main__':
     unittest.main()
