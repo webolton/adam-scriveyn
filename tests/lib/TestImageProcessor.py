5d58d26faefe05db90f6192d28af6ad9493e73e5
William Bolton <william.ellet@gmail.com>
2018-06-05 20:23:47 -0400

    Test for first folio pagination function

--- tests/lib/TestImageProcessor.py
+++ tests/lib/TestImageProcessor.py
@@ -3,7 +3,10 @@ import os
 import glob
 import imageio
 from lib import ImageProcessor
-from lib.ImageProcessor import split_and_rename_image, rename_images_in_directory, front_matter_file_names
+from lib.ImageProcessor import (
+                                split_and_rename_image, rename_images_in_directory,
+                                front_matter_file_names, first_leaf_file_names
+                                )
 
 class TestImageProcessor(unittest.TestCase):
 
@@ -23,6 +26,35 @@ class TestImageProcessor(unittest.TestCase):
         returned_names = ('N_FM_6', 'N_FM_7')
 
         self.assertEqual((returned_names, (8, 9)), (file_names, fm_numbers))
+
+    def test_first_leaf_file_names(self):
+        folio_sides = ('r', 'v')
+        start_folio = 2
+        fm_numbers = (1, 2)
+        siglum = 'N'
+        returned_r_names = (f"{siglum}_FM_1", f"{siglum}_{start_folio}r")
+        returned_v_names = (f"{siglum}_{start_folio}v", f"{siglum}_{start_folio + 1}r")
+
+        for start_side in folio_sides:
+            if start_side == 'r':
+                leaf_numbers = (1, 2)
+                file_names = returned_r_names
+                next_leaf = 2
+
+                actual_leaf_numbers, actual_file_names, actual_next_leaf = first_leaf_file_names(
+                                                                           start_side, start_folio,
+                                                                           fm_numbers, siglum)
+            if start_side == 'v':
+                leaf_numbers = (2, 3)
+                file_names = returned_v_names
+                next_leaf = 3
+
+                actual_leaf_numbers, actual_file_names, actual_next_leaf = first_leaf_file_names(
+                                                                           start_side, start_folio,
+                                                                           fm_numbers, siglum)
+
+            self.assertEqual((leaf_numbers, file_names, next_leaf),
+                            (actual_leaf_numbers, actual_file_names, actual_next_leaf))
 
     def test_split_and_rename_image(self):
         img_path = 'tests/mock_data/Matz.jpg'
